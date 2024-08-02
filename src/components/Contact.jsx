dc04cb6201efbe2978346d2f38d946059eeecbc0
Pi Rey <pilar.rrdgz@gmail.com>
2024-08-02 16:57:12 +0200

    continue with games style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,38 @@
-
-
 function Contact() {
   return (
-    <div id="contact">Contact</div>
-  )
+    <section className="contact">
+      <ul className="contact--ul">
+        <li>
+          <a
+            className="contact--ul__a"
+            href="mailto:pilar.rrdgz@gmail.com"
+            target="_blank"
+          >
+            email
+          </a>
+        </li>
+
+        <li>
+          <a
+            className="contact--ul__a"
+            href="https://github.com/Pi-Rey"
+            target="_blank"
+          >
+            github
+          </a>
+        </li>
+        <li>
+          <a
+           className="contact--ul__a"
+            href="https://www.linkedin.com/in/pilar-rey-rodriguez/"
+            target="_blank"
+          >
+            linkedin
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
